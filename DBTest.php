0c5a8684acdae24f1df17bbe06ec10e500603435
Haipeng <njucs_wuhaipeng@163.com>
2015-07-14 17:24:52 +0800

    add daocloud ci

---
+++ DBTest.php
@@ -0,0 +1,43 @@
+require_once("DB.php");
+
+class DBTest extends PHPUnit_Framework_TestCase
+{
+    public $db;
+
+    function setUp()
+    {
+        $this->db = new DB();
+    }
+
+    function tearDown() {
+        unset($this->db);
+    }
+
+    function exist($name, $phone) {
+        $contacts = $db->all();
+        foreach ($contacts as $index => $contact) {
+            if ($contact['name'] == $name && $contact['phone'] == $phone) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function total() {
+        return count($db->all());
+    }
+
+    function test001()
+    {
+        $db->add("abc", "123");
+        $this->assertTrue($this->exist("abc", "123"));
+    }
+
+    function test002()
+    {
+        $pre = $this->total();
+        $db->add("bcd", "1234");
+        $post = $this->total();
+        $this->assertTrue($post - $pre == 1);
+    }
+}
