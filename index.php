8922cb0c99a3c7a8162ae066eaa180f70e122792
Vimi <admin@loveav.me>
2017-04-10 18:14:38 +0800

    docker
    go

--- index.php
+++ index.php
@@ -1,94 +1,33 @@
-<?php
-
-require('DB.php');
-
-$db = new DB();
-
-if ($_POST) {
-    $db->add($_POST['name'], $_POST['phone']);
-}
-if ($_GET['delete']) {
-    $db->remove($_GET['delete']);
-}
-
-$contacts = $db->all();
-?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>PHP-Apache-Mysql-Sample 示例 - DaoCloud</title>
-
-    <link href="//cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100%;
-            color: #666;
-            display: table;
-        }
-
-        .container {
-            display: table-cell;
-            text-align: center;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./node_modules/normalize.css/normalize.css">
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-<div class="container">
-    <div class="content">
-        <h1>
-            PHP-Apache-Mysql-Sample 示例
-        </h1>
-        <table class="table">
-            <caption>通讯录</caption>
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>姓名</th>
-                <th>电话</th>
-                <th>操作</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php foreach ($contacts as $index => $contact) {
-                ?>
-                <tr>
-                    <th scope="row"> <?php echo $index + 1 ?></th>
-                    <td><?php echo $contact['name'] ?></td>
-                    <td><?php echo $contact['phone'] ?></td>
-                    <td>
-                        <a href="index.php?delete=<?php echo $contact['id'] ?>">删除</a>
-                    </td>
-                </tr>
-            <?php
-            } ?>
-            </tbody>
-        </table>
-
-        <div class="text-left">
-            <form method="post">
-                <div class="form-group">
-                    <label for="name">姓名</label>
-                    <input type="text" name="name" class="form-control" id="name" placeholder="姓名">
-                </div>
-                <div class="form-group">
-                    <label for="phone">号码</label>
-                    <input type="text" name="phone" class="form-control" id="phone" placeholder="号码">
-                </div>
-                <button type="submit" class="btn btn-success">新增</button>
-            </form>
-        </div>
-    </div>
-</div>
+    <h1 class="text-center">����</h1>
+    <ul class="main ovh">
+         <?php 
+         for($i=1;$i<=30;$i++){
+             <li><img src="images/loading.gif" width="256" height="256" data-original="images/$i.png"></li>
+             }
+      ?>
+    </ul>
+    <script src="./node_modules/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="./node_modules/lazyload/jquery.lazyload.js" type="text/javascript"></script>
+    <script>
+        $(window).load(
+            function(){
+                $("img").lazyload({
+                    effect: "fadeIn",
+                    threshold :250,
+                    });
+            }
+        )
+    </script>
 </body>
 </html>
