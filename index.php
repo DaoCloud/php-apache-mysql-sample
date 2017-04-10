a498db6ea93335ab93b09c7e562d12b9785b2284
Vimi <admin@loveav.me>
2017-04-10 20:11:06 +0800

    2
    2

--- index.php
+++ index.php
@@ -11,41 +11,13 @@
 <body>
     <h1 class="text-center">测试</h1>
     <ul class="main ovh">
-     <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/1.png"></li>
-           <li><img src="images/loading.gif" width="256" height="256" data-original="images/2.png"></li>    
-             
+    <?php 
+
+for ($i = 1; $i <= 10; $i++) { 
+echo "<li><img src="images/loading.gif" width="256" height="256" data-original="images/$i.png"></li>"; 
+} 
+     ?>
+        
              
     </ul>
     <script src="./node_modules/jquery/dist/jquery.min.js" type="text/javascript"></script>
