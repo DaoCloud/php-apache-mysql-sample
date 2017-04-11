73e4cf2a10fea33df7d5a75482130af4ea792fa4
Vimi <admin@loveav.me>
2017-04-11 21:23:14 +0800

    9
    9

--- index.php
+++ index.php
@@ -19,8 +19,11 @@
 
 for ($i = 1; $i <= 930; $i++) 
 { 
-   $str="<li><img src=\"images/loading.gif\" width=\"256\" data-original=\"images/".$i.".png\"></li></br><h4 class="text-center">编号为".$i."号</h4>";
+   $str="<li><img src=\"images/loading.gif\" width=\"256\" data-original=\"images/".$i.".png\"></li>“;
+   $ech="<h4 class="text-center">编号为".$i."号</h4>
    echo $str;
+   echo "</br>";
+   echo $ech;
 } 
   ?>
         
