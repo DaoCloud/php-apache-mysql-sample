e2d8c8a2958efdff383c46c4f480f86dc28eadb9
Vimi <admin@loveav.me>
2017-04-11 23:31:55 +0800

    111
    11111

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 for ($i = 1; $i <= 930; $i++) 
 { 
    $str="<li><img src=\"images/loading.gif\" width=\"256\" data-original=\"images/".$i.".png\"></li>“;
-   $ech="<h4 class=`text-center>编号为".$i."号</h4>";
+   $ech="<h4 class=\"text-center\">编号为".$i."号</h4>";
    echo $str;
    echo "</br>";
    echo $ech;
